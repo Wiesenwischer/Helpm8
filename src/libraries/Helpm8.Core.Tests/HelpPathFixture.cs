dc5c172eccf1c610cf832af12077891a85570eee
Stefan Vogelsang <s.vogelsang@ams-erp.com>
2022-07-06 14:51:11 +0200

    Should not be mergablke.

--- src/libraries/Helpm8.Core.Tests/HelpPathFixture.cs
+++ src/libraries/Helpm8.Core.Tests/HelpPathFixture.cs
@@ -39,5 +39,11 @@ namespace Helpm8.Core.Tests
             Assert.Equal(":", HelpPath.GetParentPath("::key"));
             Assert.Equal("parent", HelpPath.GetParentPath("parent:key"));
         }
+
+        [Fact]
+        public void ShouldFail()
+        {
+            Assert.True(false);
+        }
     }
 }
