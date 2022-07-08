6ad67f31c0207c1f66713087dc53640ebd1d80ba
Marcus Dammann <m.dammann@ams-erp.com>
2022-07-08 11:59:20 +0200

    Add enumeration extension methods for help class

---
+++ src/libraries/Helpm8.Core/HelpExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helpm8
+{
+    public static  class HelpExtensions
+    {
+        /// <summary>
+        /// Get the enumeration of key value pairs within the <see cref="IHelp" />
+        /// </summary>
+        /// <param name="help">The help to enumerate.</param>
+        /// <returns>An enumeration of key value pairs.</returns>
+        public static IEnumerable<KeyValuePair<string, string?>> AsEnumerable(this IHelp help) => help.AsEnumerable(makePathsRelative: false);
+
+        /// <summary>
+        /// Get the enumeration of key value pairs within the <see cref="IHelp" />
+        /// </summary>
+        /// <param name="help">The help to enumerate.</param>
+        /// <param name="makePathsRelative">If true, the child keys returned will have the current help's Path trimmed from the front.</param>
+        /// <returns>An enumeration of key value pairs.</returns>
+        public static IEnumerable<KeyValuePair<string, string?>> AsEnumerable(this IHelp help, bool makePathsRelative)
+        {
+            var stack = new Stack<IHelp>();
+            stack.Push(help);
+            int prefixLength = (makePathsRelative && help is IHelpSection rootSection) ? rootSection.Path.Length + 1 : 0;
+            while (stack.Count > 0)
+            {
+                IHelp config = stack.Pop();
+                // Don't include the sections value if we are removing paths, since it will be an empty key
+                if (config is IHelpSection section && (!makePathsRelative || config != help))
+                {
+                    yield return new KeyValuePair<string, string?>(section.Path.Substring(prefixLength), section.Value);
+                }
+                foreach (IHelpSection child in config.GetChildren())
+                {
+                    stack.Push(child);
+                }
+            }
+        }
+    }
+}
