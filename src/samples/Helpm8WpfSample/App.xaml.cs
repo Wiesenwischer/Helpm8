7b4dcaa8643fc2fd740b590a8163875fad550691
Marcus Dammann <m.dammann@ams-erp.com>
2022-07-13 07:56:09 +0200

    Improve help sample to use json and attached properties

--- src/samples/Helpm8WpfSample/App.xaml.cs
+++ src/samples/Helpm8WpfSample/App.xaml.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Helpm8;
+using Helpm8.Json;
+using Helpm8.Wpf;
 
 namespace Helpm8WpfSample
 {
@@ -13,5 +16,35 @@ namespace Helpm8WpfSample
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var help = InitializeHelp();
+            var viewModel = new MainWindowViewModel()
+            {
+                Help = help
+            };
+
+            var view = new MainWindow();
+            view.DataContext = viewModel;
+            view.Show();
+        }
+
+        private IHelp InitializeHelp()
+        {
+            var builder = new HelpBuilder();
+            builder.AddJsonFile("help.json");
+            return builder.Build();
+        }
+    }
+
+    public class MainWindowViewModel : ISupportHelp
+    {
+        public IHelp Help { get; set; }
+
+        public string Title { get; } = "Moinsen";
+
+        public string HelpKey { get; } = "MyHelpKey";
+
+
     }
 }
