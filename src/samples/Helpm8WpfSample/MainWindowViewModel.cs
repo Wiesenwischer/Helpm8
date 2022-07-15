3b732035d7bc827b7703dc59f5de71af77cde359
Marcus Dammann <m.dammann@ams-erp.com>
2022-07-15 11:24:54 +0200

    Extract MainWindowViewModel into own file

---
+++ src/samples/Helpm8WpfSample/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using Helpm8;
+using Helpm8.Wpf;
+
+namespace Helpm8WpfSample
+{
+    public class MainWindowViewModel : IHelpCommandObserver, INotifyPropertyChanged
+    {
+        private bool _isHelpActive;
+        public MainWindowViewModel()
+        {
+            
+        }
+
+        public IHelp Help { get; set; }
+
+        public string Title { get; } = "Helpm8 - WPF Sample App";
+
+        public bool IsHelpActive
+        {
+            get => _isHelpActive;
+            set
+            {
+                if (value != _isHelpActive)
+                {
+                    _isHelpActive = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsHelpActive)));
+                }
+            }
+            
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
