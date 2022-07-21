1c18d7c98fdceac05e0b2e95a70a3cdaf824d43a
Marcus Dammann <m.dammann@ams-erp.com>
2022-07-21 12:49:15 +0200

    Improving tests and threshold up to 90%

---
+++ src/libraries/Helpm8.Core.Tests/ChainedHelpProviderExtensionsFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Helpm8.Core.Tests
+{
+    public class ChainedHelpProviderExtensionsFixture
+    {
+        [Fact]
+        public void ThrowsOnNulls()
+        {
+            var builder = new HelpBuilder();
+            var root = builder.Build();
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                ChainedBuilderExtensions.AddHelp(null, root, true);
+            });
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                ChainedBuilderExtensions.AddHelp(builder, null, true);
+            });
+        }
+    }
+}
